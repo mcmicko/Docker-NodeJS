c3c720f50b2b2061708a053989b7a2c08e57fc3c
mcmicko <mcmzrc@gmail.com>
2021-08-11 20:51:22 +0200

    Protect Endpoint

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
 const {
   MONGO_IP,
   MONGO_PORT,
   MONGO_USER,
+  REDIS_URL,
   MONGO_PASSWORD,
+  REDIS_PORT,
+  SESSION_SECRET,
 } = require("./config/config");
+let RedisStore = require("connect-redis")(session);
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  post: REDIS_PORT,
+});
 
 const postRouter = require("./routes/postRoutes");
 const userRouter = require("./routes/userRoutes");
@@ -30,6 +40,20 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+//MIDDLEWARE
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      gttpOnly: true,
+      maxAge: 3000000,
+    },
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
